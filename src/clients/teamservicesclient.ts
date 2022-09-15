7baa12da3a370e3709591e9e04fb5077fd63992b
Liran Dobrish <liran_dobrish@hotmail.com>
2022-09-15 22:49:24 +0300

    updated tfsclient "validateTfvcCollectionUrl" method

--- src/clients/teamservicesclient.ts
+++ src/clients/teamservicesclient.ts
@@ -5,10 +5,12 @@
 "use strict";
 
 import { IRequestHandler } from "azure-devops-node-api/interfaces/common/VsoBaseInterfaces";
-import { IRestResponse } from "typed-rest-client/RestClient";
+import { IRestResponse, IRequestOptions } from "typed-rest-client/RestClient";
 import { LocationsApi } from "azure-devops-node-api/LocationsApi";
 import { ConnectionData } from "azure-devops-node-api/interfaces/LocationsInterfaces";
 import { ClientApiBase } from "azure-devops-node-api/ClientApiBases";
+import { TfvcBranch, TypeInfo } from "azure-devops-node-api/interfaces/TfvcInterfaces";
+import { ClientVersioningData } from "azure-devops-node-api/VsoClient";
 
 export class TeamServicesApi extends ClientApiBase {
     handlers: IRequestHandler[];
@@ -37,20 +39,34 @@ export class TeamServicesApi extends ClientApiBase {
         return promise;
     }
 
-    //Used to determine if the baseUrl points to a valid TFVC repository
-    public async validateTfvcCollectionUrl(): Promise<any> {
-        //Create an instance of Promise since we're calling a function with the callback pattern but want to return a Promise
-        const promise: Promise<any> = new Promise<any>(async (resolve, reject) => {
-            /* tslint:disable:no-null-keyword */
-            const response: IRestResponse<any> = await this.rest.get<any>(this.vsoClient.resolveUrl("_apis/tfvc/branches")); //, "", null, null, (err: any, statusCode: number, obj: any) => {
-            /* tslint:enable:no-null-keyword */
-            if (response.statusCode !== 200) {
-                reject(response);
-            } else {
-                resolve(response);
+    public async validateTfvcCollectionUrl(): Promise<boolean> {
+        return new Promise<boolean>(async (resolve, reject) => {
+            try {
+                let routeValues: any = {
+                    project: ""
+                };
+
+                let verData: ClientVersioningData = await this.vsoClient.getVersioningData(
+                    "5.0",
+                    "tfvc",
+                    "bc1f417e-239d-42e7-85e1-76e80cb2d6eb",
+                    routeValues);
+
+                let url: string = verData.requestUrl!;
+                let options: IRequestOptions = this.createRequestOptions('application/json', verData.apiVersion);
+
+                let res: IRestResponse<TfvcBranch[]>;
+                res = await this.rest.get<TfvcBranch[]>(url, options);
+
+                let ret = this.formatResponse(res.result,
+                    TypeInfo.TfvcBranch,
+                    true);
+
+                resolve(ret !== undefined);
+            }
+            catch (err) {
+                reject(false);
             }
         });
-        return promise;
     }
-
 }
